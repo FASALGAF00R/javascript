d5cd7a7b8454db549dc83944e125ba964669083b
FASALGAFOOR <fasalgafoor2080@gmail.com>
2024-04-03 11:59:15 +0530

    working on bascicd

--- difffunctions.js
+++ difffunctions.js
@@ -1,27 +1,76 @@
 
 
-(function(){
-    let t=9
-    console.log(t);
-})()
+// (function(){
+//     let t=9
+//     console.log(t);
+// })()
 
-function a(){
-    let i=8
-    console.log(i);
-}
-a()
+// function a(){
+//     let i=8
+//     console.log(i);
+// }
+// a()
 
 
-const s=[1,2,3,4,5,9,12,15]
-for(let i=0;i<s.length;i++){
-    if(s[i]%3==0){
-        for(let k=i;k<s.length;k++){
-            s[k]=s[k+1]
-            k--;
-        }
-    }
-}
-console.log(s);
+// function reversearray(arr){
+//     if(arr.length<=1){
+//         return arr
+//     }
+
+//     return reversearray(arr.slice(1)).concat(arr[0])
+
+// }
+
+// const arr=[1,2,3,4,5]
+// const res=reversearray(arr)
+// console.log(res);
+
+
+// const s='leveluuu'
+// const c=s.split('')
+
+// function a(c){
+//     for(let i=0;i<c.length/2;i++){
+//         if(c[i]==c[c.length-1-i]){
+//             return true
+//         }else{
+//             return false
+//         }
+//     }
+// }
+// const res=a(c)
+// console.log(res);
+
+
+// const s='level'
+// const c=s.split('')
+
+// function palindrome(c){
+//     if(c.length<=1){
+//         return true
+//     }
+//     if(c[0]==c[c.length-1]){
+//         return palindrome(c.slice(1,-1))
+//     }else{
+//         return false
+//     }
+
+// }
+// console.log(palindrome(c));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
