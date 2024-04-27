72de46b5967c85c9fd439d076a85ced687694e5d
FASALGAFOOR <fasalgafoor2080@gmail.com>
2024-04-27 17:47:10 +0530

    sgdf:

---
+++ math.js
@@ -0,0 +1,24 @@
+// console.log(Math.round(4.6))
+// console.log(Math.ceil(4.9))
+// console.log(Math.floor(4.9))
+// console.log(Math.trunc(4.9))
+// console.log(Math.sign(-14))
+// console.log(Math.pow(4, 2))
+// console.log(Math.sqrt(4))
+// console.log(Math.abs(-5.6))
+// console.log(Math.min(0, 150, 30, 20, -8, -200))
+// console.log(Math.max(0, 150, 30, 20, -8, -200))
+
+
+// // get random numbers
+
+// function getRandomInt(min, max) {
+//     min = Math.ceil(min);
+//     max = Math.floor(max);
+//     return Math.floor(Math.random() * (max - min + 1)) + min;
+// }
+
+// var randomInt = getRandomInt(1, 100);
+// console.log(randomInt);
+
+// print 10 numbers from 1 to 100
