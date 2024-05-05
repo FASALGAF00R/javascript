896866eef2a0d95b841070e302ca9a638d142719
FASALGAFOOR <fasalgafoor2080@gmail.com>
2024-05-05 21:23:21 +0530

    fjhf

--- closure.js
+++ closure.js
@@ -14,3 +14,26 @@
 
 //   console.log(myFunction());
 
+function isPrime(num) {
+    if (num <= 1) {
+        return false;
+    }
+    if (num <= 3) {
+        return true;
+    }
+    if (num % 2 === 0 || num % 3 === 0) {
+        return false;
+    }
+    let i = 5;
+    while (i * i <= num) {
+        if (num % i === 0 || num % (i + 2) === 0) {
+            return false;
+        }
+        i += 6;
+    }
+    return true;
+}
+
+// Example usage
+console.log(isPrime(11)); // true
+console.log(isPrime(12)); // false
