999efab50df32793ec52cd18280c0ce8728f3466
FASALGAFOOR <fasalgafoor2080@gmail.com>
2024-10-20 10:07:24 +0530

    value change

--- arrays.js
+++ arrays.js
@@ -1,6 +1,6 @@
 const a = [1, 1, 2, 2, 4]
 let count
-let f = []
+let g = []
 for (let i = 0; i < a.length; i++) {
     count = 1
     if (a[i] != -1) {
@@ -10,10 +10,10 @@ for (let i = 0; i < a.length; i++) {
                 a[j] = -1
             }
         }
-        f.push({vl:a[i],fre:count})
+        g.push({vl:a[i],fre:count})
     
 }
 }
-console.log(f);
+console.log(g);
 
 
