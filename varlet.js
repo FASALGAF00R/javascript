d41c63a8c116368cf50aa94bd11ab642a9995409
FASALGAFOOR <fasalgafoor2080@gmail.com>
2025-03-16 18:02:47 +0530

    covered

--- varlet.js
+++ varlet.js
@@ -16,7 +16,63 @@
 // console.log(person.fullname);
 
 
+// objects, arrays, dates, maps, sets, intarrays, floatarrays, promises, and more.
 
 
+// var
+
+// var name="fasal"
+// var name="gafoor"
+// name="kk"
+// console.log(name);
+
+// if (true) {
+//     var age = 30;
+// }
+// console.log(age); 
+ 
+// let
+// let name="ds"
+//  name="oo"
+// console.log(name);
+
+// if (true) {
+// let age=30
+// console.log(age); 
+// }
+
+
+// function a(b){
+//     console.log(b);
+    
+// }
+// a("how are u")
+
+// let =myfunction(3,4)
+
+// function myfunction(a,b){
+// return a+b;
+
+// }
+//     const person=new Object()
+//     person.name="fasal gafoor";
+//     person.age=56
+//     person.place="cherukara"
+
+// console.log(person);
+
+const person = {
+    firstName:"John",
+    lastName:"Doe",
+    age:50,
+     eyeColor:"blue"
+  }
+  
+
+
+//   const x = person;
+  
+//   console.log(x)
+//   x.age = 10;
 
 
